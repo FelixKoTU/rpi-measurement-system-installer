923a7819343afbc72267dcdebfb6834f676c45c5
FelixKoTU <github@customail.net>
2024-03-06 23:00:37 +0100

    add dummy move cmds to installer script; add tarball

--- installer.sh
+++ installer.sh
@@ -7,11 +7,26 @@ apt-get update
 apt-get upgrade -y
 #apt-get install $PACKAGES -y
 
-# Download files (wget and unzip)
-
 # Enable ssh
 
 # Move files to correct directories
+USER="pidb"
+
+python_source_dir="/home/$USER/Documents/rpi-installer/Code" 
+python_destination_dir="/home/$USER/code
+
+html_source_dir="/home/$USER/Documents/rpi-installer/html"
+html_destination_dir="/var/www/html"
+
+echo "Moving files."
+mv "$python_source_dir"/* "$python_destination_dir/"
+
+# check if Apache2 user has been set up correctly
+# check if html_dest_dir exists
+
+mv "$html_source_dir"/* "$html_destination_dir/"
+
+echo "Files moved succesfully
 
 # Set visudo option
 
