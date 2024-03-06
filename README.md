539f97e352f5c6f4a60709439ff526a574ad4f54
FelixKoTU <github@customail.net>
2024-03-06 19:17:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rapsberry Pi installer
+This release handles all the necessary packages and configurations for the measurement system.
+
+Run the following files with:
+```sh
+sudo sh installer.sh
+./dummy.sh
+```
---
+++ installer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# installer.sh installs the necessary packages and dependencies for the measurement system
+
+# Install packages
+#PACKAGES="php sqlite3 php-sqlite3"
+apt-get update
+apt-get upgrade -y
+#apt-get install $PACKAGES -y
+
+# Download files (wget and unzip)
+
+# Enable ssh
+
+# Move files to correct directories
+
+# Set visudo option
+
+# Enable php
