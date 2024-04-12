b63f3d6d1211eb5c3062ccb45afd31b43bea3567
FelixKoTU <github@customail.net>
2024-04-12 13:43:23 +0200

    refactor installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # installer.sh handles packages, dependencies, user permissions, system configurations for the rpi measurement system.
 echo "This shell script installer is used to set up a Raspberry Pi Model 2B after installing the OS and enabling ssh connection via the rpi-imager."
-echo "\n"
+echo
 
 # ------
 # Install packages
@@ -13,21 +13,22 @@ apt-get install $PACKAGES -y
 
 # ------
 # Enable ssh
-echo "Enable ssh"
+echo "Enable SSH"
 systemctl enable ssh
 systemctl start ssh
-if sudo systemctl is-active --quiet ssh; then
+if systemctl is-active --quiet ssh; then
     echo "SSH has been enabled successfully."
 else
     echo "SSH not enabled. Please check the configuration manually by typing 'sudo raspi-config'."
 fi
-echo "\n"
+echo
 
 # ------
 # Parse current user into variable 
 USER=$(who am i | awk '{print $1}')
-echo "\n"
-## Alternatively query user input 
+echo
+
+# Alternatively query user input 
 # prompt_input() {
 #     read -p "Please enter your username (non root user): " USER
 #     echo "You entered: $USER"
@@ -42,23 +43,19 @@ echo "\n"
 # done
 
 # echo "Username entered successfully!"
-# echo "\n"
+# echo
 
 # ------
 # Move files to correct directories
 echo "Move files to correct directories"
-
-PYTHON_SOURCE_DIR="./Code/Python" 
+PYTHON_SOURCE_DIR="./Code/Python"
 PYTHON_DESTINATION_DIR="/home/$USER/code"
-
 HTML_SOURCE_DIR="./Code/html"
 HTML_DESTINATION_DIR="/var/www/html"
-
 echo "Creating directories"
 mkdir -p "$PYTHON_DESTINATION_DIR"
 mkdir -p "$HTML_DESTINATION_DIR"
-
-echo "Moving files."
+echo "Moving files"
 mv "$PYTHON_SOURCE_DIR"/* "$PYTHON_DESTINATION_DIR/"
 mv "$HTML_SOURCE_DIR"/* "$HTML_DESTINATION_DIR/"
 if [ "$(ls -A "$PYTHON_DESTINATION_DIR")" ]; then
@@ -71,14 +68,13 @@ if [ "$(ls -A $HTML_DESTINATION_DIR)" ]; then
 else
     echo "Error while moving files. Check destination directory: ${HTML_DESTINATION_DIR}."
 fi
-echo "\n"
+echo
 
 # ------
 # Set visudo permissions
 echo "Set visudo permissions"
 CUSTOM_CMDS_LINE="Cmnd_Alias CUSTOM_CMDS = /usr/bin/pkill, /usr/sbin/shutdown -h now, /usr/sbin/shutdown -r now"
 WWW_DATA_LINE="www-data ALL = NOPASSWD: CUSTOM_CMDS"
-# Check if the lines already exist in the sudoers file
 if ! grep -Fxq "$CUSTOM_CMDS_LINE" /etc/sudoers && ! grep -Fxq "$WWW_DATA_LINE" /etc/sudoers; then
     echo "$CUSTOM_CMDS_LINE" | sudo tee -a /etc/sudoers > /dev/null 2>&1
     echo "$WWW_DATA_LINE" | sudo tee -a /etc/sudoers > /dev/null 2>&1
@@ -86,62 +82,56 @@ if ! grep -Fxq "$CUSTOM_CMDS_LINE" /etc/sudoers && ! grep -Fxq "$WWW_DATA_LINE" 
 else
     echo "visudo lines already exist in /etc/sudoers."
 fi
-echo "\n"
+echo
 
 # ------
-# Apache2 webserver
-# check if Apache2 user has been set up correctly and cofigure all necessary files
+# Apache2 web server
 HTACCESS_FILE="/var/www/html/.htaccess"
 HTPASSWD_FILE="/home/$USER/.htpasswd"
-
-# create the .htpasswd file with user password
+# Create the .htpasswd file with user password
 echo "Choose Apache2 website password"
 htpasswd -cB "$HTPASSWD_FILE" "$USER"
-
-# rewrite .htaccess file
+# Rewrite .htaccess file
 echo "AuthUserFile $HTPASSWD_FILE" >> "$HTACCESS_FILE"
 echo "AuthType Basic" >> "$HTACCESS_FILE"
 echo 'AuthName "My restricted Area"' >> "$HTACCESS_FILE"
 echo "Require valid-user" >> "$HTACCESS_FILE"
 echo "ErrorDocument 404 /404.html" >> "$HTACCESS_FILE"
-# enable php in html
+# Enable php in html
 echo "AddHandler application/x-httpd-php .html" >> "$HTACCESS_FILE"
-echo "\n"
+echo
 
-# adjust '/etc/apache2/apache2.conf' file and restart web server
+# Adjust '/etc/apache2/apache2.conf' file and restart web server
 echo "Adjust '/etc/apache2/apache2.conf' file and restart web server."
 sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' "/etc/apache2/apache2.conf"
 a2enmod rewrite
 service apache2 restart
-echo "\n"
+echo
 
-# check if apache2 web server is running
-servstat=$(service apache2 status)
-if echo "$servstat" | grep -q "active (running)"; then
-    echo "Apache2 web server set up succesfully. Paste the local IP address into the web browser of the RPi."
-    echo "Local IP adress: $(hostname -I)"
+# Check if apache2 web server is running
+if service apache2 status | grep -q "active (running)"; then
+    echo "Apache2 web server set up successfully. Paste the local IP address into the web browser of the RPi."
+    echo "Local IP address: $(hostname -I)"
 else
     echo "Apache2 is not running."
 fi
-echo "\n"
+echo
 
 # ------
 # Set USB permissions
-usermod -a -G dialout www-data
+usermod -aG dialout www-data
 
 # ------
 # Initialize DB and chmod and group of IoT.db
 echo "Create sqlite3 db in '/home/$USER/code/' and set various file and owner permissions."
 usermod -aG pidb www-data
-mkdir /home/$USER/code/logs
+mkdir -p /home/$USER/code/logs
 cd /home/$USER/code
 python3 /home/$USER/code/initialize_DB_Tables.py
 cd -
-chown pidb:pidb /home/$USER/code/
-chown pidb:pidb /home/$USER/code/*
-chmod 774 /home/$USER/code/
-chmod 774 /home/$USER/code/*
-echo "\n"
+chown pidb:pidb /home/$USER/code/ /home/$USER/code/* 
+chmod -R 774 /home/$USER/code/
+echo
 
 # ------
 # Reboot
